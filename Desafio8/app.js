b35ae6fcb3b8ce06ce1946a5caf888df76fe0682
Alejo Greco <uki.greco@gmail.com>
2022-09-04 17:56:44 -0300

    Estructura de ruta productos y levantar server

---
+++ Desafio8/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const productsRoute = require('./routes/products')
+
+const app = express()
+
+const server = app.listen(8080, () => console.log('Server up!'))
+
+server.on('error', e => console.log(e))
+
+app.use(express.json())
+app.use('api/productos', productsRoute)
