2b78db70843cfa0a80c8eb853b47c5e2fe26c040
Alejo Greco <uki.greco@gmail.com>
2022-09-12 20:51:18 -0300

    Creando ruta para tratar las peticiones de productos

--- Desafio10/handlebars/app.js
+++ Desafio10/handlebars/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const app = express()
+const { route : productosRoute, productos } = require('./routes/productos')
 
+const app = express()
 const server = app.listen(8080, () => console.log('Server handlebars running!'))
 
 app.engine('handlebars', handlebars.engine())
 
 app.set('views', './views')
 app.set('view engine', 'handlebars')
+
+app.use('/productos', productosRoute)
 
---
+++ Desafio10/routes/productos.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const route = express.Router()
+
+const productos = []
+
+route.get('/', (req, res) => {
+
+})
+
+route.post('/', (req, res) => {
+    
+})
+
+module.exports = {route, productos}
