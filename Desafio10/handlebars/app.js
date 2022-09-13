e3ef15bf6119160f3d8e24dc3fd044ba5ac05b67
Alejo Greco <uki.greco@gmail.com>
2022-09-12 21:23:04 -0300

    Implementacion del post y get a ruta raiz

--- Desafio10/handlebars/app.js
+++ Desafio10/handlebars/app.js
@@ -5,10 +5,15 @@ const { route : productosRoute, productos } = require('./routes/productos')
 const app = express()
 const server = app.listen(8080, () => console.log('Server handlebars running!'))
 
+app.use(express.urlencoded())
 app.engine('handlebars', handlebars.engine())
 
 app.set('views', './views')
 app.set('view engine', 'handlebars')
 
 app.use('/productos', productosRoute)
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
 
--- Desafio10/handlebars/routes/productos.js
+++ Desafio10/handlebars/routes/productos.js
@@ -4,11 +4,21 @@ const route = express.Router()
 const productos = []
 
 route.get('/', (req, res) => {
-
+    res.render('productos', {
+        productos
+    })
 })
 
 route.post('/', (req, res) => {
-    
+    if(req.body){
+        productos.push(req.body)
+        console.log(productos)
+        res.redirect('/')
+    }
+    else{
+        res.status(400).send({error: 'Datos erroneos'})
+    }
+
 })
 
 module.exports = {route, productos}
