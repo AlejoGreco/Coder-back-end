4ecc76fe24ec208287f7c790ccc8a8e5253c14e7
Alejo Greco <uki.greco@gmail.com>
2022-09-19 19:31:08 -0300

    Generacion de tabla dinamica segun msj del server ws

--- Desafio12/src/public/main.js
+++ Desafio12/src/public/main.js
@@ -1,5 +1,33 @@
 const socket = io.connect()
 
-socket.on('productos', productos => {
-    console.log(productos)
-})
+const renderProducts = data => {
+    const prodContainer = document.getElementById('productos')
+    if(data.length){
+        const bodyTable = data.map(p => (
+            `<tr>
+                <td>${p.title}</th>
+                <td>${p.price}</td>
+                <td><img src=${p.thumbnail} class="img-thumbnail rounded"></td>
+            </tr>`
+        )).join(' ')
+
+        prodContainer.innerHTML = `
+        <table class="table table-light table-striped table-borderless table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Imagen</th>
+                </tr>productos
+            </thead>
+            <tbody>
+                ${bodyTable}
+            </tbody>
+        </table>`
+    }
+    else{
+        prodContainer.innerHTML = '<div class="alert alert-info" role="alert">No hay productos dados de alta</div>'
+    }
+}
+
+socket.on('productos', productos => { renderProducts(productos) })
