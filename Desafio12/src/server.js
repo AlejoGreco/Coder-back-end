a2a7891b81ee1c6e96d2185e9a5133196fe39a69
Alejo Greco <uki.greco@gmail.com>
2022-09-18 23:28:27 -0300

    Levantando servidor websocket y carpeta public

--- Desafio12/src/server.js
+++ Desafio12/src/server.js
@@ -1,7 +1,19 @@
 const express = require('express')
-
+const { Server } = require('socket.io')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 const server = app.listen(PORT, () => console.log(`Server up! It's running in port ${PORT}`))
+
+const io = new Server(server)   // Creamos servidor tcp para sockets
+
+app.use(express.static('./src/public'))
+
+let productos = []
+
+io.on('connection', socket => {
+    console.log(`Nuevo cliente conectado`)
+    //socket.emit('productos', productos)
+
+})
