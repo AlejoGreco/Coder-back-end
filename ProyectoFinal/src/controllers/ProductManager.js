28807c4e2f82629ba98a6e850ef8ec80685f87c6
Alejo Greco <uki.greco@gmail.com>
2022-09-25 23:57:27 -0300

    Moddificando manejo de errores para manejar en un solo lugar

--- ProyectoFinal/src/controllers/ProductManager.js
+++ ProyectoFinal/src/controllers/ProductManager.js
@@ -6,100 +6,83 @@ class ProductManager {
     }
 
     async getProducts(){
+        if(!fs.existsSync(this.path)){
+            throw { error : -3, descripcion : 'El archivo de productos no existe' }
+        }
+
         try {
-            if(fs.existsSync(this.path)){
-                return JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
-            }
-            return { error : -3, descripcion : 'El archivo de productos no existe' }
-        } catch (e) {
-            return { error : -99, descripcion : 'Error al leer el archivo de productos' }
+            return JSON.parse(await fs.promises.readFile(this.path, 'utf-8'))
+        } 
+        catch {
+            throw { error : -98, descripcion : 'Error al leer el archivo de productos' }
         }
     }
 
     async getProduct(id){
-        try {
-            const products = await this.getProducts()
-            if(products.error){
-                return {...products}
-            }
+        const products = await this.getProducts()
 
-            const product = products.find(p => p.id === id)
-            if(product){
-                return product
-            }
-
-            return { error : -4, descripcion : `No existe el producto de id ${id}` }
-
-        } catch (e) {
-            return { error : -100, descripcion : e.message}
+        const product = products.find(p => p.id === id)
+        if(product){
+            return product
         }
+
+        throw { error : -4, descripcion : `No existe el producto de id ${id}` }
     }
 
     async createProduct(p){
         let products = []
         let newProd
 
+        if(fs.existsSync(this.path)){
+            products = await this.getProducts()
+                
+            newProd = { id : (products[products.length - 1].id + 1), timestamp : Date.now(), ...p}
+        }
+        else{
+            newProd = { id : 1, timestamp : Date.now(), ...p }
+        }
+
         try {
-            if(fs.existsSync(this.path)){
-                products = await this.getProducts()
-                if(products.error){
-                    return {...products}
-                }    
-                newProd = { id : (products[products.length - 1].id + 1), timestamp : Date.now(), ...p}
-            }
-            else{
-                newProd = { id : 1, timestamp : Date.now(), ...p }
-            }
-    
             await fs.promises.writeFile(this.path, JSON.stringify([...products, newProd], null, 2))    
             return newProd
         }
-        catch (e){
-            return {error : -100, message : 'No se pudo crear el producto'}
+        catch {
+            throw {error : -100, descripcion : 'No se pudo crear el archivo de productos'}
         }
     }
 
     async updateProduct(id, p){
+        const products = await this.getProducts()
+        const index = products.findIndex(p => p.id === id)
 
-        try{
-            const products = await this.getProducts()
-            if(products.error){
-                return {...products}
-            }
-
-            const index = products.findIndex(p => p.id === id)
-            if(index !== -1){
-                products[index] = {...products[index], ...p}
-                await fs.promises.writeFile(this.path, JSON.stringify([...products], null, 2))    
-                return products[index]
-            }
-            else{
-                return { error : -4, descripcion : `No existe el producto de id ${id}` }
-            }
+        if(index === -1){
+            throw { error : -4, descripcion : `No existe el producto de id ${id}` }
         }
-        catch (e){
-            return {error : -100, message : 'No se pudo modificar el producto'}
+
+        products[index] = {...products[index], ...p}
+        try{
+            await fs.promises.writeFile(this.path, JSON.stringify([...products], null, 2))    
+            return products[index]
+        }
+        catch {
+            throw {error : -99, descripcion : 'No se pudo modificar el archivo de productos'}
         }
     }
 
     async deleteProduct(id){
-        try {
-            const products = await this.getProducts()
-            if(products.error){
-                return {...products}
-            }
+        const products = await this.getProducts()
 
-            const index = products.findIndex(p => p.id === id)
-            if(index !== -1){
-                await fs.promises.writeFile(this.path, JSON.stringify(products.filter(p => p.id !== id), null, 2))    
-                return products[index]
-            }
-            else{
-                return { error : -4, descripcion : `No existe el producto de id ${id}` }
-            }
+        const index = products.findIndex(p => p.id === id)
+        if(index === -1){
+            throw { error : -4, descripcion : `No existe el producto de id ${id}` }
+        }
 
-        } catch (e) {
-            return {error : -100, descripcion : 'No se pudo borrar el producto'}
+        try{
+            await fs.promises.writeFile(this.path, JSON.stringify(products.filter(p => p.id !== id), null, 2))    
+            return products[index]
+        }
+        catch {
+            throw {error : -99, descripcion : 'No se pudo modificar el archivo de productos'}
         }
     }
 }
--- ProyectoFinal/src/router/productos.js
+++ ProyectoFinal/src/router/productos.js
@@ -5,28 +5,48 @@ const ProductManager = require('../controllers/ProductManager')
 const pm = new ProductManager('src/data/products.json')
 
 route.get('/', async (req, res) => {
-    const products = await pm.getProducts()
-    res.send(products)
+    try{
+        res.send(await pm.getProducts())
+    }
+    catch (e){
+        res.send(e)
+    }
 })
 
 route.get('/:id', async (req, res) => {
-    const product = await pm.getProduct(parseInt(req.params.id))
-    res.send(product)
+    try{
+        res.send(await pm.getProduct(parseInt(req.params.id)))
+    }
+    catch (e){
+        res.send(e)
+    }
 })
 
 route.post('/', async (req, res) => {
-    const product = await pm.createProduct(req.body)
-    res.send(product)
+    try{
+        res.send(await pm.createProduct(req.body))
+    }
+    catch (e){
+        res.send(e)
+    }
 })
 
 route.put('/:id', async (req, res) => {
-    const product = await pm.updateProduct(parseInt(req.params.id), req.body)
-    res.send(product)
+    try{
+        res.send(await pm.updateProduct(parseInt(req.params.id), req.body))
+    }
+    catch (e){
+        res.send(e)
+    }
 })
 
 route.delete('/:id', async (req, res) => {
-    const product = await pm.deleteProduct(parseInt(req.params.id))
-    res.send(product)
+    try{
+        res.send(await pm.deleteProduct(parseInt(req.params.id)))
+    }
+    catch (e){
+        res.send(e)
+    }
 })
 
 module.exports = route
