206c1aac42d4d92e9f606ab1022b05650bf14a9b
Alejo Greco <uki.greco@gmail.com>
2022-09-27 00:02:50 -0300

    Utilizando nuevo router y middleware para rutas no implementadas

--- ProyectoFinal/src/app.js
+++ ProyectoFinal/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const productosRoute = require('./router/productos')
+const carritosRoute = require('./router/carritos')
 
 const app = express()
 
@@ -10,5 +11,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 
 app.use('/api/productos', productosRoute)
+app.use('/api/carrito', carritosRoute)
+
+app.use((req, res) => {
+    res.status(404).send({
+        error: -2, 
+        descripcion: `Ruta ${req.baseUrl}${req.url} metodo ${req.method} no implementada`
+    })
+})
 
 server.on('error', e => console.log(e))
