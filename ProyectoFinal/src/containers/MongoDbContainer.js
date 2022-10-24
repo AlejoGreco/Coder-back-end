84c8d1d201111779cc67feccca2f77e9116fe97e
Alejo Greco <uki.greco@gmail.com>
2022-10-24 00:55:35 -0300

    Implementacion de DAO para productos mongodb

--- ProyectoFinal/src/containers/MongoDbContainer.js
+++ ProyectoFinal/src/containers/MongoDbContainer.js
@@ -3,34 +3,34 @@ class MongoDbContainer {
         this.model = model
     }
 
-    async create(req) {
-        const  doc = await this.model.create(req.body)
+    async create(body) {
+        const  doc = await this.model.create(body)
         console.log(doc)
-        return doc.json()
+        return doc
     }
     
     async readAll() {
         const docs = await this.model.find()
         console.log(docs)
-        return docs.toArray()
+        return docs
     }
 
     async readById(id) {
         const doc = await this.model.findById(id)
         console.log(doc)
-        return doc.json()
+        return doc
     }
 
-    async update(id) {
-        const doc = await this.model.findByIdAndUpdate(id, req.body)
+    async update(id, body) {
+        const doc = await this.model.findByIdAndUpdate(id, body)
         console.log(doc)
-        return doc.json()
+        return doc
     }
 
     async destroy(id) {
         const doc = await this.model.findByIdAndDelete(id)
         console.log(doc)
-        return doc.json()
+        return doc
     }
 }
 
--- ProyectoFinal/src/daos/products/ProductMongoDao.js
+++ ProyectoFinal/src/daos/products/ProductMongoDao.js
@@ -1,1 +1,69 @@
 const MongoDbContainer = require('../../containers/MongoDbContainer')
+const ProductModel = require('../../models/productModel')
+
+class ProductMongoDao extends MongoDbContainer {
+    constructor(){
+        super(ProductModel)
+    }
+
+    async getProducts(req, res){
+        try {
+            const products = await this.readAll()
+            console.log(products)
+            return res.status(200).json(products.toArray())        
+        } 
+        catch (e){
+            res.status(404).json({ message: e.message, code: e.code })
+        }
+    }
+
+    async getProduct(req, res){
+        try {
+            const { id } = req.params
+            const product = await this.readById(id)
+            console.log(product)
+            return res.status(200).json(product)
+        }
+        catch (e){
+            return res.status(404).json({ message: e.message, code: e.code })
+        }        
+    }
+
+    async createProduct(req, res){
+        try {
+            const newProd = { timestamp: Date.now(), ...req.body }
+            const result = await this.create(newProd)
+            console.log(result)
+            return res.status(200).json(result)
+        }
+        catch (e){
+            return res.status(404).json({ message: e.message, code: e.code })
+        }
+    }
+
+    async updateProduct(req, res){
+        try {
+            const { id } = req.params
+            const result = await this.update(id, req.body)
+            console.log(result)
+            return res.status(200).json({ message: 'Product updated!', productUpdated: result})
+        } 
+        catch (e){
+            return res.status(404).json({ message: e.message, code: e.code })
+        }
+    }
+
+    async deleteProduct(req, res){
+        try {
+            const { id } = req.params
+            const productDeleted = await this.destroy(id)
+            console.log(productDeleted)
+            return res.status(200).json({ message: 'Product deleted!', productDeleted})
+        }
+        catch (e){
+            return res.status(404).json({ message: e.message, code: e.code })
+        }        
+    }
+}
+
+module.exports = ProductMongoDao
