ede0f366ccb142252d006c79e4f57abef44b06b9
Alejo Greco <uki.greco@gmail.com>
2023-01-17 01:13:40 -0300

    Implementacion de ruta info para datos de users

--- ProyectoFinal/src/router/users.js
+++ ProyectoFinal/src/router/users.js
@@ -3,18 +3,17 @@ import passport from "passport";
 
 const route = Router()
 
-route.post('/register', passport.authenticate('register', 
-    {failureMessage: true}), 
+route.post('/register', passport.authenticate('register', {failureMessage: true}), 
     (req, res) => {
         console.log(req.user)
-        res.send('User Created!')
+        res.send({status: 'success', message: 'User created!'})
     }
 )
 
 route.post('/login', passport.authenticate('login', {failureMessage: true}), 
     (req, res) => {
         console.log(req.user)
-        res.send(req.session.user)
+        res.send({status: 'success', message: 'User logged in!'})
     }
 )
 
@@ -24,8 +23,25 @@ route.post('/logout', (req, res) => {
             console.log(e)
             res.send({error: e})
         }
-        res.send({status: 'success', message: 'Loged out!'})
+        console.log(req.user)
+        res.send({status: 'success', message: 'User logged out!'})
     })
+})
+
+route.get('/info', (req, res) => {
+    if(req.isAuthenticated()){
+        const user = {
+            name: req.user.name,
+            adress: req.user.adress,
+            age: req.user.age,
+            phone: req.user.phone,
+            email: req.user.emil
+        }
+        res.send({resulusert})
+    }
+    else{
+        res.status(401).send({message: 'User unknow. You must be logged for this request'})
+    }
 })
 
 export default route
