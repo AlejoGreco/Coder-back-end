c0c090aac40c21fa249c920bf1efbc222e16bb5d
Alejo Greco <uki.greco@gmail.com>
2023-01-19 20:10:45 -0300

    Ejecucion en modo cluster funcionando

--- ProyectoFinal/src/index.js
+++ ProyectoFinal/src/index.js
@@ -1,8 +1,23 @@
 import cluster from 'cluster'
+import os from 'os'
 import app from './app.js'
 import { PORT } from './config.js'
 
-// Server up
-const server = app.listen(PORT, () => console.log(`Server up! Listening at port ${PORT}`))
+const CLUSTER = false
+const cpuNumbers = os.cpus().length
 
-server.on('error', e => console.log(e))
+if(cluster.isPrimary && CLUSTER){
+    console.log(`Iniciando workers. Pid maestro: ${process.pid}`)
+    for(let i = 0; i < cpuNumbers; i++){
+        cluster.fork()
+    }
+
+    cluster.on('exit', (worker) => {
+        console.log(`Termino el worker ${worker.id}`)
+        cluster.fork()
+    })
+}
+else{
+    const server = app.listen(PORT, () => console.log(`Worker ${process.pid} iniciado. Listening at port ${PORT}`))
+    server.on('error', e => console.log(e))
+}
