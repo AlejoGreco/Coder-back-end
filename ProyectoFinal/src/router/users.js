d56d23125d886bb1715081284cd86820eb8fa7fe
Alejo Greco <uki.greco@gmail.com>
2023-01-20 00:54:20 -0300

    Imagenes y payloads actualizados

--- ProyectoFinal/src/router/users.js
+++ ProyectoFinal/src/router/users.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
+import logger from "../logger/index.js";
 import transporter, { ADMIN_EMAIL } from "../transports/mailer.js";
 import { checkAuth } from "../middlewares/auth.js";
 
