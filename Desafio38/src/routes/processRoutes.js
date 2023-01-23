112d5c4c0ba0dbe2cdd86e7a394fcb12cef9ef27
Alejo Greco <uki.greco@gmail.com>
2023-01-23 01:56:55 -0300

    Controller para ruta process

--- Desafio38/src/routes/processRoutes.js
+++ Desafio38/src/routes/processRoutes.js
@@ -1,24 +1,9 @@
 import { Router } from "express"
-import { getProcessData, performHeavyProcess } from "../services/process.services.js"
+import { getRandomDataController, getProcessDataController } from "../controllers/process.controller.js"
 
 const route = Router()
 
-route.get('/info', (req, res) => {    
-    const processData = getProcessData()
-
-    res.render('info', {
-        ...processData
-    })
-})
-
-route.get('/randoms', async (req, res) => {
-    const amount = req.query.amount ? parseInt(req.query.amount) : 100000000
-    const result = await performHeavyProcess(amount)
-
-    res.render('random', {
-        result,
-        length: 1000
-    })
-})
+route.get('/info', getProcessDataController)
+route.get('/randoms', getRandomDataController)
 
 export default route
