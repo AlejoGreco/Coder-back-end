930767f529b232382f935a572df09f08b2ac4eb5
Alejo Greco <uki.greco@gmail.com>
2023-02-07 23:40:34 -0300

    Estructura de los test basicos a realizar con mocha

---
+++ ProyectoFinal/src/test/auto.test.js
@@ -0,0 +1,44 @@
+
+
+describe('Test API - Entidad Productos', () => {
+
+    describe('Peticiones POST | Crear productos', () => {
+        it('Se incrementa la logitud del arreglo de productos al crear uno', async () => {
+
+        })
+
+        it('Debe retornar un producto con los parametros enviados', async () => {
+
+        })
+    })
+
+    describe('Peticiones GET | Obtener productos', () => {
+        it('La petición debería retornar status 200', async () => {
+            
+        })
+
+        it('La petición debería retornar un array de producto o array vacio', async () => {
+            
+        })
+
+        it('La petición debería retornar un producto al especificar id', async () => {
+            
+        })
+    })
+
+    describe('Peticiones PUT | Editar productos', () => {
+        it('Debo obtener un producto editado con los nuevos parametros', async () => {
+
+        })
+    })
+
+    describe('Peticiones DELETE | Eliminar productos', () => {
+        it('Debe decrementar en 1 la longitud del arreglo de productos al eliminar', async () => {
+
+        })
+
+        it('No debo encontrar un producto eliminado', async () => {
+
+        })
+    })
+})
