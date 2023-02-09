73b30044e7e51744dda851898a45c4de5b3f7ba4
Alejo Greco <uki.greco@gmail.com>
2023-02-08 23:02:41 -0300

    Get test to api using mocha

--- ProyectoFinal/src/test/auto.test.js
+++ ProyectoFinal/src/test/auto.test.js
@@ -1,43 +1,71 @@
+import supertest from "supertest"
+import { expect } from "chai"
+import { config, auth } from './config.test.js'
 
+const request = supertest(`http://${config.host}:${config.port}`)
 
 describe('Test API - Entidad Productos', () => {
+    let cookies
+    before(async () => {
+        // Logueo de usuario y guardado de cookie de session
+        const res = await request.post(`/${auth.path}`).send(auth.credentials)
+        cookies = res.headers['set-cookie'].toString().split(';')[0].concat(';')
+    })
 
-    describe('Peticiones POST | Crear productos', () => {
-        it('Se incrementa la logitud del arreglo de productos al crear uno', async () => {
+    describe('POST', () => {
 
+        it('Se deberia agregar un producto', async () => {
+            const body = {
+                nombre: "Producto test 1",
+                descripcion: "Soy producto de prueba manual",
+                url: "www.test.com/manual.jpeg",
+                codigo: 1000,
+                stock: 50,
+                precio: 77.6
+            }
+
+            const res = await request.post('/api/productos').set('Cookie', [cookies]).send(body)
+            expect(res.status).to.equal(200)
+
+            const {_id, __v, ...newBody} = res.body.newItem
+            // verifico keys del schema
+            expect(res.body.newItem).to.include.keys('nombre', 'descripcion', 'url', 'codigo', 'stock', 'precio', '_id')
+            // verifico values
+            expect(newBody).to.deep.equal(body)
+            
         })
 
-        it('Debe retornar un producto con los parametros enviados', async () => {
+    })
+
+    describe('GET', () => {
+
+        it('Debería retornar un array de producto o array vacio', async () => {
+            const res = await request.get('/api/productos').set('Cookie', [cookies])
+            expect(res.status).to.equal(200)
+            expect(Array.isArray(res.body)).to.equal(true)
+        })
+
+        it('Debería retornar el producto indicado al especificar id', async () => {
+            let res = await request.get('/api/productos').set('Cookie', [cookies])
+            expect(res.status).to.equal(200)
+            
+            const id = res.body[0]._id
+            const { body } = await request.get(`/api/productos/${id}`).set('Cookie', [cookies])
+            // verifico schema de producto
+            expect(body.item).to.include.keys('nombre', 'descripcion', 'url', 'codigo', 'stock', 'precio', '_id')
+            // verifico mismo id que solicite
+            expect(body.item._id).to.equal(id)
+        })
+    })
+
+    describe('PUT', () => {
+        it('Debo actualizar un producto existente', async () => {
 
         })
     })
 
-    describe('Peticiones GET | Obtener productos', () => {
-        it('La petición debería retornar status 200', async () => {
-            
-        })
-
-        it('La petición debería retornar un array de producto o array vacio', async () => {
-            
-        })
-
-        it('La petición debería retornar un producto al especificar id', async () => {
-            
-        })
-    })
-
-    describe('Peticiones PUT | Editar productos', () => {
-        it('Debo obtener un producto editado con los nuevos parametros', async () => {
-
-        })
-    })
-
-    describe('Peticiones DELETE | Eliminar productos', () => {
-        it('Debe decrementar en 1 la longitud del arreglo de productos al eliminar', async () => {
-
-        })
-
-        it('No debo encontrar un producto eliminado', async () => {
+    describe('Peticiones DELETE', () => {
+        it('Debe quitar el producto especificado del arreglo de productos', async () => {
 
         })
     })
---
+++ ProyectoFinal/src/test/config.test.js
@@ -0,0 +1,12 @@
+export const config = {
+    host: 'localhost', 
+    port: 8080
+}
+  
+export  const auth = {
+    path: 'user/login',
+    credentials: {
+      "username": "trici1@gmail.com",
+      "password": "12345"
+    }
+}
