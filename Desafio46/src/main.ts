876bec875e33b51a43a93ddb3bdea5a7e47bfaf6
Alejo Greco <uki.greco@gmail.com>
2023-02-19 22:59:25 -0300

    Creando entidad productos y agregando Swagger

--- Desafio46/src/main.ts
+++ Desafio46/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Coder API ')
+    .setDescription('This api is the first example about how to use Nest JS')
+    .setVersion('1.0')
+    .addTag('Products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
