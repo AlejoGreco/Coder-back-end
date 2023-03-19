edb50f40b437f46c6e754a0119b355cc88fd432b
Alejo Greco <uki.greco@gmail.com>
2023-03-19 16:48:40 -0300

    Errores para Productos

--- ProyectoFinal/src/daos/products/ProductMongoDao.js
+++ ProyectoFinal/src/daos/products/ProductMongoDao.js
@@ -2,6 +2,7 @@ import MongoDbContainer from '../../containers/MongoDbContainer.js'
 import ProductSchema from '../../schemas/productSchema.js'
 import mongoose from 'mongoose'
 import { CONNECTION_STR } from '../../config.js'
+import ErrorDto from '../../dtos/ErrorDto.js'
 
 class ProductMongoDao {
     constructor(dbName){
@@ -14,23 +15,83 @@ class ProductMongoDao {
     }
 
     async getAllProducts(){
-        return await this.container.read()
+        try{
+            const products = await this.container.read()
+            if(!products)
+                throw new ErrorDto({params: {}},'No se pudo obtener productos', 400, -20)
+            
+            return products
+        }
+        catch (e){
+            if(!e.error?.params){
+                return new ErrorDto(e, 'No se pudo obtener productos | Lanzado por aplicacion', 400, -200)
+            }
+            return e
+        }
     }
 
     async getProduct(id){
-        return await this.container.read(id)
+        try{
+            const product = await this.container.read(id)
+            if(!product)
+                throw new ErrorDto({params: {id}},'No se pudo obtener producto', 400, -21)
+            
+            return product
+        }
+        catch (e){
+            if(!e.error?.params){
+                return new ErrorDto(e, 'No se pudo obtener producto | Lanzado por aplicacion', 400, -210)
+            }
+            return e
+        }
     }
 
     async createProduct(product){
-        return this.container.create(product)
+        try{
+            const result = await this.container.create(product)
+            if(!result)
+                throw new ErrorDto({params: {product}},'No se pudo crear producto', 400, -22)
+            
+            return result
+        }
+        catch (e){
+            if(!e.error?.params){
+                return new ErrorDto(e, 'No se pudo crear producto | Lanzado por aplicacion', 400, -220)
+            }
+            return e
+        }
     }
 
-    async updateProduct(id, product){
-        return this.container.update(id, product)
+    async updateProduct(id, product){ 
+        try{
+            const result = await this.container.update(id, product)
+            if(!result)
+                throw new ErrorDto({params: {id, product}},'No se pudo actualizar el producto', 400, -23)
+            
+            return result
+        }
+        catch (e){
+            if(!e.error?.params){
+                return new ErrorDto(e, 'No se pudo actualizar producto | Lanzado por aplicacion', 400, -230)
+            }
+            return e
+        }
     }
 
     async deleteProduct(id){
-        return await this.container.destroy(id)
+        try{
+            const result = await this.container.destroy(id)
+            if(!result)
+                throw new ErrorDto({params: {id}},'No se pudo eliminar el producto', 400, -24)
+            
+            return result
+        }
+        catch (e){
+            if(!e.error?.params){
+                return new ErrorDto(e, 'No se pudo eliminar producto | Lanzado por aplicacion', 400, -240)
+            }
+            return e
+        }
     }
 }
 
